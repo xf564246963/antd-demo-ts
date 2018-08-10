0d6131fcb5aa4339dc2dce9878a02ca088b596e7
xiongfeng <xiongfeng@qiyi.com>
2018-08-10 17:08:01 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
 import * as React from 'react';
-import Button from 'antd/lib/button';
-import './App.css';
+import { Layout, Menu, Icon } from 'antd';
+import "./App.css";
 
-class App extends React.Component {
+const { Header, Sider, Content } = Layout;
+
+class SiderDemo extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    toggle(){
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
     render() {
         return (
-            <div className="App">
-                <Button type="primary" htmlType="button">Button</Button>
-            </div>
+            <section id="components-layout-demo-custom-trigger">
+                <Layout style={{ height: "100%" }}>
+                    <Sider
+                        trigger={null}
+                        collapsible={true}
+                        collapsed={this.state.collapsed}
+                    >
+                        <div className="logo" />
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">
+                                <Icon type="user" />
+                                <span>nav 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="video-camera" />
+                                <span>nav 2</span>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Icon type="upload" />
+                                <span>nav 3</span>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }}>
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle.bind(this)}
+                            />
+                        </Header>
+                        <Content style={{ background: '#fff'}}>
+                            Content
+                    </Content>
+                    </Layout>
+                </Layout>
+            </section>
         );
     }
 }
 
-export default App;
+
+export default SiderDemo;
